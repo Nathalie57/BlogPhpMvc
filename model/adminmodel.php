96c554dd12407f87eb2810afe51d15fab7e88dd6
Nathalie57 <57112509+Nathalie57@users.noreply.github.com>
2019-12-30 16:58:48 +0100

    Accès interface admin

---
+++ model/adminmodel.php
@@ -0,0 +1,26 @@
+<?php
+require_once "model/model.php";
+
+class AdminModel extends Model{
+
+public $info;
+
+	public function __construct($login, $password){
+		parent::__construct();
+		$this->login($login, $password);
+	}
+
+public function login($login, $password){
+  $sql = "SELECT 
+  			idAdmin, 
+  			login  
+
+  			FROM `admin` 
+  			WHERE `login` = '$login' AND `password` = '$password'";
+  
+  $data = $this->db->query($sql);
+  
+  $this->info = $data->fetch();
+}
+
+}
