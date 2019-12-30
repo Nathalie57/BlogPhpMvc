209d6478a6793651cce47542f38d49fc0ef23b22
Nathalie57 <57112509+Nathalie57@users.noreply.github.com>
2019-12-30 16:25:08 +0100

    Finalisation

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
-<?php
-require_once "controller/front.php";
-require_once "controller/back.php";
+	<?php
+	require_once "controller/front.php";
+	require_once "controller/back.php";
+	require_once "controller/security.php";
 
-$config = [
-"directory" => "Projet%203b",
-"database" => "projet3",
-"user" => "root",
-"password" => "",
-"envProd" => false
-];
-$uri = explode ("/", filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL, FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_LOW));
-$uri = array_slice($uri, array_search($config["directory"], $uri)+1);
-switch ($uri[0]) {
-	case 'admin':
+	$config = [
+		"directory" => "Projet%203b",
+		"database" => "projet3",
+		"user" => "root",
+		"password" => "",
+		"envProd" => false
+	];
+
+	$safeData = new Security([
+
+		"post" => [
+			"author"  => FILTER_SANITIZE_STRING,
+			"comment" => FILTER_SANITIZE_STRING,
+			"login" => FILTER_SANITIZE_STRING,
+			"password" => FILTER_SANITIZE_STRING,
+			"title" => FILTER_SANITIZE_STRING,
+			"content" => FILTER_DEFAULT,
+			"slug" => FILTER_SANITIZE_STRING,
+			"idChapitre" => FILTER_SANITIZE_STRING,
+			"ID" => FILTER_SANITIZE_STRING,
+		]
+	]
+);
+
+	$uri = $safeData->uri;
+	
+	switch ($uri[0]) {
+		case 'admin':
 		$page = new Back(array_slice($uri, 1));
 		break;
-	
-	default:
+		
+		default:
 		$page = new Front($uri);
 		break;
-}
-echo $page->html;
+	}
+	echo $page->html;
